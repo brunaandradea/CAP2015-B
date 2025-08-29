5267967c52eeef76425b78bb6b1d77cbf575ee64
Bruna Andrade Alves <179890@unifio.edu.br>
2025-08-29 01:42:11 +0000

    Aula dia 28/08/2025

---
+++ src/main/java/app/Atividade.java
@@ -0,0 +1,12 @@
+package app;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Atividade {
+    private String descricao;
+
+    public abstract String getDetalhes();
+}
